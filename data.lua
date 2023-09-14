9c9a5c40e51a091f73c8a715bb5533ff37794d5f
critipan <1524506135@qq.com>
2023-09-14 18:48:05 +0800

    在启用 ironman_wm和se时更改盔甲的合成表

--- data.lua
+++ data.lua
@@ -1,17 +1,33 @@
+-- 用于按需加载模组物品
+
 -- 加载配置文件
 require("config")
 
 -- 标志区，用于指示可选mod是否存在
-flag_ironman = data.raw.item["arc-reactor"] ~= nil
-
-
+local flag_ironman = mods["IronMan_WarMachine"] ~= nil
+local flag_se = mods["space-exploration"] ~= nil
 
 
 -- 执行加载操作
-
+-- 启用钢铁侠mod
 if flag_ironman then
     
 else
     require("prototypes.arc-reactor")
     require("prototypes.advanced-exoskeleton")
+end
+
+
+-- 启用钢铁侠和se
+if flag_ironman and flag_se then
+
+
+    -- 替换钢铁侠盔甲的合成表
+    data.raw.recipe["iron-armor"].ingredients = {
+        {"se-thruster-suit-4", 1},
+        {"low-density-structure", 40},
+        {"electric-engine-unit", 40},
+        {"processing-unit", 100},
+        {"alien-biomatter", 500}
+       }
 end
